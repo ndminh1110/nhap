5d9d6349d23cdfe6b77d52e695fa17b9b2c5ccfa
ndminh <minh_dth235698@student.agu.edu.vn>
2025-11-05 15:08:47 +0700

    Add files via upload

---
+++ SQL_QuanLyTuyenDuLich_Python.sql
@@ -0,0 +1,100 @@
+﻿CREATE DATABASE QuanLyTuyenDuLich
+
+ON
+(
+	name = 'QuanLyTuyenDuLich_data',
+	filename = 'D:\DoAn\QuanLyTuyenDuLich_data.mdf',
+	size = 8MB,
+	maxsize = 100MB,
+	filegrowth = 2MB)
+LOG ON
+(
+	name = 'QuanLyTuyenDuLich_log',
+	filename = 'D:\DoAn\QuanLyTuyenDuLich_log.ldf',
+	size = 8MB,
+	maxsize = 100MB,
+	filegrowth = 2MB);
+GO
+
+USE QuanLyTuyenDuLich;
+GO
+
+--TAO BANG--
+CREATE TABLE KHACHHANG
+(
+	maKH varchar(6) check ( maKH like 'KH[0-9][0-9][0-9][0-9]'),
+	hoTen nvarchar(40),
+	phai nvarchar(3),
+	ngsinh datetime,
+	sdt varchar(10) check ( sdt like '[0][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+	dchi nvarchar(50),
+	primary key (maKH)
+);
+
+CREATE TABLE NHANVIEN
+(
+	maNV varchar(6) check (maNV like ''),
+	hoTen nvarchar(40),
+	chucVu nvarchar(20),
+	phai nvarchar(3),
+	ngsinh datetime,
+	sdt varchar(10) check ( sdt like '[0][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+	dchi nvarchar(50),
+	luong float,
+	primary key (maNV)
+);
+
+CREATE TABLE TUYENDULICH
+(
+	ddDen nvarchar(50),
+	ddDi nvarchar(50),
+	maTuyen varchar(6) check (maTuyen like ''),
+	primary key (maTuyen)
+);
+
+CREATE TABLE CHUYENDI
+(
+	ngKh datetime,
+	tgKh time,
+	maCD varchar(6),
+	maNV varchar(6),
+	maTuyen varchar(6),
+	primary key (maCD)
+);
+--chua co khoa chinh
+CREATE TABLE DATVE
+(
+	maVe varchar(20),
+	maKH varchar(6),
+	maCD varchar(6),
+	ngDat datetime,
+	trangThai nvarchar(13),
+	thanhTien float,
+	giaVe float,
+	soLuong int
+);
+
+CREATE TABLE DOANHTHU
+(
+	maCD varchar(6),
+	giaVe float,
+	tongTien float,
+	ngayLap datetime
+);
+
+--LienKet SQL va Python
+CREATE TABLE TaiKhoan 
+(
+    TenDangNhap NVARCHAR(50) PRIMARY KEY,
+    MatKhau NVARCHAR(50) NOT NULL,
+    VaiTro NVARCHAR(20) NOT NULL  -- 'QuanLy' hoặc 'NhanVien'
+);
+GO
+
+INSERT INTO TaiKhoan (TenDangNhap, MatKhau, VaiTro)
+VALUES 
+('quanly', 'taolaquanly', 'QuanLy'),
+('nhanvien', 'emchaosep', 'NhanVien');
+GO
+
+--DROP DATABASE QuanLyTuyenDuLich
